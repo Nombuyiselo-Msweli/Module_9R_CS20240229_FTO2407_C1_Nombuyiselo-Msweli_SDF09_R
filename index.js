c3f946beb1e751fd3165e9f3c558c733842dd12d
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-07-31 15:53:19 +0200

    updated index.js + index.html with functionality to display card number

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector("#sum-el") - another way to do the same thing
+let cardsEl = document.getElementById("cards-el")
 
     function startGame() {
+        cardsEl.textContent = "Cards:" + firstCard + "+" + secondCard
         sumEl.textContent = "Sum:" + sum
 
         if (sum <= 20){
