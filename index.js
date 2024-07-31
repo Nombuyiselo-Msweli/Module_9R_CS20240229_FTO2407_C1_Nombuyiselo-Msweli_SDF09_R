dd7097b62f9e829d8431e6fb88dadf2012db9492
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-01 00:05:52 +0200

    ipdated index.js with operational condition on getting a new card

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 
-//Setting conditional statement for sum of cards
-let firstCard = 9
-let secondCard = 5
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+//DECLARING VARIABLES
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 //let sumEl = document.querySelector("#sum-el") - another way to do the same thing
 let cardsEl = document.getElementById("cards-el")
 
+// BASE GAME FUNCTIONS
+    function getRandomCard(){
+        let randomNumber =  Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10){
+            return 10
+        }
+    else if (randomNumber === 1){
+        return 11
+    }
+    else {
+        return randomNumber
+        }
+    }
+
+
     function startGame() {
+        isAlive = true
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+
         renderGame()
     }
     function renderGame() {
 
-        cardsEl.textContent = "Cards:" 
+        cardsEl.textContent = "Cards:"                              //for loop allows me to render all three cards within the array
         for (let i = 0; i < cards.length; i++) {
             cardsEl.textContent += cards[i] + " "
         }
@@ -39,13 +58,13 @@ let cardsEl = document.getElementById("cards-el")
         messageEl.textContent = message
     }
 
+
+        //NEW CARD FUNCTION
     function newCard(){
-
-        let card = 10
-
+        if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
         sum += card
-        cards.push(card)
-
+        cards.push(card)                       //allows me to add the new card to the array
         renderGame()
-        console.log(cards)
+        }
     }
