6750bdc0ca3ac2334c8020fc9c5b3fc17ef3a7a1
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-07-31 12:16:11 +0200

    updated index.js with if else conditions for the cards draw with messages on player status

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 6
+//Setting conditional statement for sum of cards
+let firstCard = 8
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-console.log(sum)
+if (sum <= 20){
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    hasBlackJack = true 
+    message = "Wohoo! You've got Blackjack! 🥳"
+} else {
+    isAlive = false
+    "You're out of the game! 😭"
+}
+
+//CASH OUT condition 
+//console.log(hasBlackJack)
+//console.log(isAlive)
+console.log(message)
